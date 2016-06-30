1ed1eb2ddface9e244e8dedba270ee2395874042
Amy McNeill <amymcneill@Amys-MacBook-Pro.local>
2016-06-30 12:28:20 +0900

    Initial Commit

---
+++ pata/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pata
+//
+//  Created by Amy McNeill on 6/30/16.
+//  Copyright (c) 2016 Amy McNeill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
